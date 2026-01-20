2944ec253e2da6c2b31ef3b3d4c541c6dbb7bff3
Charles Jester <charles.jester@revature.com>
2026-01-20 12:55:42 -0600

    updated json

--- src/agent.py
+++ src/agent.py
@@ -13,8 +13,8 @@ Documentation: https://docs.langchain.com/langsmith/application-structure
 from dotenv import load_dotenv
 from langgraph.graph import StateGraph, START, END
 
-from supervisor_agent.utils.state import SupervisorState
-from supervisor_agent.utils.nodes import (
+from src.utils.state import SupervisorState
+from src.utils.nodes import (
     supervisor_node,
     calendar_node,
     email_node,
